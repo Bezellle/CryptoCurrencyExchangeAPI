2cd34f0db90cc5e94f867387582cb79de149d377
Bezellle <bazyl.kupka@gmail.com>
2021-08-08 01:24:42 +0200

    Update service for new request logic

--- src/main/java/com/example/currancyexchange/CryptoExchangeService.java
+++ src/main/java/com/example/currancyexchange/CryptoExchangeService.java
@@ -2,10 +2,8 @@ package com.example.currancyexchange;
 
 import com.example.currancyexchange.domain.CryptoExchangeResponse;
 import com.example.currancyexchange.domain.CurrancyRequest;
-import com.example.currancyexchange.domain.NomicCombinedResponse;
-import com.example.currancyexchange.domain.RatesCryptoExchangeResponse;
+import com.example.currancyexchange.domain.RequestedRates;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,17 +13,22 @@ public class CryptoExchangeService {
 
     private final CryptoExchangeClient cryptoExchangeClient;
     private final CryptoExchangeResponseMapper cryptoExchangeResponseMapper;
+    private final CryptoExchangeCalculator cryptoExchangeCalculator;
 
-    public CryptoExchangeService(CryptoExchangeClient cryptoExchangeClient, CryptoExchangeResponseMapper cryptoExchangeResponseMapper) {
+    public CryptoExchangeService(CryptoExchangeClient cryptoExchangeClient,
+                                 CryptoExchangeResponseMapper cryptoExchangeResponseMapper,
+                                 CryptoExchangeCalculator cryptoExchangeCalculator) {
         this.cryptoExchangeClient = cryptoExchangeClient;
         this.cryptoExchangeResponseMapper = cryptoExchangeResponseMapper;
+        this.cryptoExchangeCalculator = cryptoExchangeCalculator;
     }
 
-    public CryptoExchangeResponse getCurrancyResponse(String currency,List<String> filters){
+    public CryptoExchangeResponse getCurrancyResponse(String currency, List<String> filters) {
         CurrancyRequest[] currancyRequests = cryptoExchangeClient.getNomicCombinesResponseList();
-        RatesCryptoExchangeResponse ratesCryptoExchangeResponse = cryptoExchangeResponseMapper.createMapWithRates(currancyRequests);
-        return cryptoExchangeResponseMapper.getResponse(currancyRequests, filters);
+        RequestedRates requestedRates = cryptoExchangeResponseMapper.createMapWithRates(currancyRequests);
+
+        return cryptoExchangeCalculator.calculateResponseRates(requestedRates, currency, filters);
     }
 }
 
-// TODO Validation, Exception, Calculator
+// TODO Validation, Exception
