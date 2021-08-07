0a164138b66073d3f79353e870477670c0de5f06
Bezellle <bazyl.kupka@gmail.com>
2021-08-08 00:39:14 +0200

    Add EmbeddedTomcatCustomizer to allow brackets in URL

--- src/main/java/com/example/currancyexchange/EmbeddedTomcatCustomizer.java
+++ src/main/java/com/example/currancyexchange/EmbeddedTomcatCustomizer.java
@@ -1,4 +1,16 @@
 package com.example.currancyexchange;
 
-public class EmbededTomcatCustomizer {
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+
+public class EmbeddedTomcatCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+            connector.setProperty("relaxedPathChars", "<>[\\]^`{|}");
+            connector.setProperty("relaxedQueryChars", "<>[\\]^`{|}");
+        });
+    }
 }
