d55bdca1586f46a295b025f02fda7bb051421993
Bezellle <bazyl.kupka@gmail.com>
2021-08-08 01:22:46 +0200

    Update Mapper for new API request

--- src/main/java/com/example/currancyexchange/CryptoExchangeResponseMapper.java
+++ src/main/java/com/example/currancyexchange/CryptoExchangeResponseMapper.java
@@ -2,28 +2,18 @@ package com.example.currancyexchange;
 
 import com.example.currancyexchange.domain.CryptoExchangeResponse;
 import com.example.currancyexchange.domain.CurrancyRequest;
-import com.example.currancyexchange.domain.NomicCombinedResponse;
-import com.example.currancyexchange.domain.RatesCryptoExchangeResponse;
+import com.example.currancyexchange.domain.RequestedRates;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Component
 public class CryptoExchangeResponseMapper {
 
-    public CryptoExchangeResponse getResponse(CurrancyRequest[] currancyRequests, List<String> filters){
-        // pass
-        return new CryptoExchangeResponse();
-    }
-
-    public RatesCryptoExchangeResponse createMapWithRates( CurrancyRequest[] currancyRequests){
-        RatesCryptoExchangeResponse ratesMap = new RatesCryptoExchangeResponse();
+    public RequestedRates createMapWithRates(CurrancyRequest[] currancyRequests){
+        RequestedRates ratesMap = new RequestedRates();
         ratesMap.setRates(Arrays.stream(currancyRequests).collect(Collectors.toMap(CurrancyRequest::getCurrency, CurrancyRequest::getRate)));
         return ratesMap;
     }
