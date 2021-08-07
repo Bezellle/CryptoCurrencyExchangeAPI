791913374d4dd5905d453355a4a2c39ba6b49a9d
Bezellle <bazyl.kupka@gmail.com>
2021-08-08 00:40:11 +0200

    Add CurrancyExchangeApplication

--- src/main/java/com/example/currancyexchange/CurrancyExchangeApplication.java
+++ src/main/java/com/example/currancyexchange/CurrancyExchangeApplication.java
@@ -2,6 +2,11 @@ package com.example.currancyexchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CurrancyExchangeApplication {
@@ -10,4 +15,14 @@ public class CurrancyExchangeApplication {
         SpringApplication.run(CurrancyExchangeApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> containerCustomizer() {
+
+        return new EmbeddedTomcatCustomizer();
+    }
 }
