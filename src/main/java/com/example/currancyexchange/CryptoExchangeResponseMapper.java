1951032b287a7ecc19de70ac0dbf9b511735c9f6
Bezellle <bazyl.kupka@gmail.com>
2021-08-08 03:40:04 +0200

    Add object mapping for exchange final response

--- src/main/java/com/example/currancyexchange/CryptoExchangeResponseMapper.java
+++ src/main/java/com/example/currancyexchange/CryptoExchangeResponseMapper.java
@@ -1,10 +1,10 @@
 package com.example.currancyexchange;
 
-import com.example.currancyexchange.domain.CryptoExchangeResponse;
-import com.example.currancyexchange.domain.CurrancyRequest;
-import com.example.currancyexchange.domain.RequestedRates;
+import com.example.currancyexchange.domain.*;
 import org.springframework.stereotype.Component;
 
+import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,5 +16,13 @@ public class CryptoExchangeResponseMapper {
         RequestedRates ratesMap = new RequestedRates();
         ratesMap.setRates(Arrays.stream(currancyRequests).collect(Collectors.toMap(CurrancyRequest::getCurrency, CurrancyRequest::getRate)));
         return ratesMap;
+    }
+
+    public ExchangeResponse createExchangeResponse(ExchangeRequestBody exchangeRequestBody, ArrayList<ExchangeResult> results){
+        ExchangeResponse exchangeResponse = new ExchangeResponse();
+        exchangeResponse.setFrom(exchangeRequestBody.getFrom());
+        exchangeResponse.setAmount(exchangeRequestBody.getAmount());
+        exchangeResponse.setExchangeResultList(results);
+        return exchangeResponse;
     }
 }
