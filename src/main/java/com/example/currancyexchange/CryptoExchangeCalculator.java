0581a65874da58ea667c8da1aac354aa0b956eec
Bezellle <bazyl.kupka@gmail.com>
2021-08-08 03:38:03 +0200

    Add exchange calculation

--- src/main/java/com/example/currancyexchange/CryptoExchangeCalculator.java
+++ src/main/java/com/example/currancyexchange/CryptoExchangeCalculator.java
@@ -1,11 +1,14 @@
 package com.example.currancyexchange;
 
 import com.example.currancyexchange.domain.CryptoExchangeResponse;
+import com.example.currancyexchange.domain.ExchangeRequestBody;
+import com.example.currancyexchange.domain.ExchangeResult;
 import com.example.currancyexchange.domain.RequestedRates;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -17,12 +20,29 @@ public class CryptoExchangeCalculator {
         result.setSource(currency);
         if (filters != null && !filters.isEmpty()) {
             for (String filter : filters) {
-                BigDecimal rate = rates.getRates().get(currency).divide(rates.getRates().get(filter), RoundingMode.HALF_UP);
+                BigDecimal rate = calculateRate(rates, currency, filter);
                 result.getRates().put(filter, rate);
             }
         } else {
             result.getRates().put(defaultExchangeCurrency, rates.getRates().get(currency));
         }
         return result;
+    }
+
+    private BigDecimal calculateRate(RequestedRates rates, String currencyFrom, String currencyTo) {
+        return rates.getRates().get(currencyFrom).divide(rates.getRates().get(currencyTo), RoundingMode.HALF_UP);
+    }
+
+    public ArrayList<ExchangeResult> calculateExchangeResults(RequestedRates requestedRates, ExchangeRequestBody exchangeRequestBody) {
+        ArrayList<ExchangeResult> exchangeResults = new ArrayList<ExchangeResult>();
+        for (String currencyTo : exchangeRequestBody.getTo()) {
+            ExchangeResult result = new ExchangeResult();
+            result.setCurrency(currencyTo);
+            result.setRate(calculateRate(requestedRates, exchangeRequestBody.getFrom(), currencyTo));
+            result.setResult(exchangeRequestBody.getAmount().multiply(BigDecimal.valueOf(ExchangeResult.getFee())).multiply(result.getRate()));
+
+            exchangeResults.add(result);
+        }
+        return exchangeResults;
     }
 }
